f3e8d04e46ba153de7d9ba79efc0ee11f2e0db98
NAL Koulu <niko.a.leinonen@gmail.com>
2020-08-17 14:47:34 +0300

    Revert "Fixed slider defaulting to 0"
    This reverts commit 428d16fd1d1d8a3a1d44140a65579dc05a712849.

--- KoponenDatingSimulator/KDS/UI.py
+++ KoponenDatingSimulator/KDS/UI.py
@@ -20,10 +20,10 @@ class New:
         8. handle_pressed_color (OPTIONAL): The color of the handle in idle state. [DEFAULT: (90, 90, 90)]
         """
 
-        def __init__(self, safe_name, slider_rect, handle_size: (int, int), default_value=0.0, handle_move_area_padding=(0, 0), slider_color=(120, 120, 120), handle_default_color=(100, 100, 100), handle_highlighted_color=(115, 115, 115), handle_pressed_color=(90, 90, 90)):
+        def __init__(self, safe_name, slider_rect, handle_size: (int, int), handle_move_area_padding=(0, 0), slider_color=(120, 120, 120), handle_default_color=(100, 100, 100), handle_highlighted_color=(115, 115, 115), handle_pressed_color=(90, 90, 90)):
             self.safe_name = safe_name
             self.slider_rect = slider_rect
-            self.handle_rect = pygame.Rect(slider_rect.midleft[0] + (float(KDS.ConfigManager.LoadSetting("Settings", safe_name, str(default_value))) * slider_rect.width) - (handle_size[0] / 2), 0, handle_size[0], handle_size[1])
+            self.handle_rect = pygame.Rect(slider_rect.midleft[0] + (float(KDS.ConfigManager.LoadSetting("Settings", safe_name, "0")) * slider_rect.width) - (handle_size[0] / 2), 0, handle_size[0], handle_size[1])
             self.handle_move_area_padding = handle_move_area_padding
             self.slider_color = slider_color
             self.handle_default_color = handle_default_color
