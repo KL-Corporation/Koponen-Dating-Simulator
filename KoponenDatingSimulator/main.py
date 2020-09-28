89b89dcefc2612a475ef9d129722825e74d26f5b
penkkihiomakone <67598180+penkkihiomakone@users.noreply.github.com>
2020-09-28 17:52:54 +0300

    Slight improvement

--- KoponenDatingSimulator/main.py
+++ KoponenDatingSimulator/main.py
@@ -2686,7 +2686,7 @@ def main_menu():
 
             display.blit(main_menu_title, (391, 43))
             framecounter += 1
-            if framecounter > 1000:
+            if framecounter > 600:
                 current_frame += 1
                 framecounter = 0
                 if current_frame > len(frames)-1:
