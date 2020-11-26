07cd4ad119caef40bca1265a0c24415b5730c35c
NAL <niko.a.leinonen@gmail.com>
2020-11-26 15:46:24 +0200

    Fixed error showing on .map file load

--- KoponenDatingSimulator.py
+++ KoponenDatingSimulator.py
@@ -507,7 +507,7 @@ class WorldData():
         global Items, tiles, Enemies, Projectiles
         MapPath = os.path.join("Assets", "Maps", "map" + current_map)
         PersistentMapPath = os.path.join(PersistentPaths.CachePath, "map")
-        if not ( os.path.isdir(MapPath) and os.path.isfile(os.path.join(MapPath, "level.dat")) and os.path.isfile(os.path.join(MapPath, "levelprop.kdf")) and os.path.isfile(os.path.join(MapPath, "music.ogg")) ):
+        if not os.path.isfile(MapPath + ".map") and not (os.path.isdir(MapPath) and os.path.isfile(os.path.join(MapPath, "level.dat")) and os.path.isfile(os.path.join(MapPath, "levelprop.kdf")) and os.path.isfile(os.path.join(MapPath, "music.ogg"))):
             KDS.Logging.AutoError(f"""##### MAP FILE ERROR #####
 Map Directory: {os.path.isdir(MapPath)}
 Level File: {os.path.isfile(os.path.join(MapPath, "level.dat"))}
