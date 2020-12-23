1c0c3a47de1445310e28e84e9306e71443ef5616
NAL <niko.a.leinonen@gmail.com>
2020-12-23 12:34:03 +0200

    Added KDS.Events (untested)

--- KDS/Missions.py
+++ KDS/Missions.py
@@ -75,7 +75,7 @@ class Task:
         Missions.GetMission(missionName).AddTask(safeName, self)
         
     def Progress(self, Value: float, Add: bool = False):
-        self.progress = self.progress + Value if Add else Value
+        self.progress = (self.progress + Value if Add else Value)
         if self.progress >= 1.0:
             self.finished = True
             self.progress = 1.0
