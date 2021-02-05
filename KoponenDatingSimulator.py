68ca6daf87674249ada10a703f3b12c9f0e9cd8f
NAL <niko.a.leinonen@gmail.com>
2021-02-05 19:45:48 +0200

    Update KoponenDatingSimulator.py

--- KoponenDatingSimulator.py
+++ KoponenDatingSimulator.py
@@ -580,7 +580,7 @@ class Inventory:
     doubleItem = "doubleItem"
 
     def __init__(self, size: int):
-        self.storage: List[Union[Item, str]] = [Inventory.emptySlot for _ in range(size)]
+        self.storage: List[Item | str] = [Inventory.emptySlot for _ in range(size)]
         self.size: int = size
         self.SIndex: int = 0
 
