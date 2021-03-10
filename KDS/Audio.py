815656854722e57f87fdce359b1554aa433cc7d2
NAL <niko.a.leinonen@gmail.com>
2021-03-10 11:10:56 +0200

    Update Audio.py

--- KDS/Audio.py
+++ KDS/Audio.py
@@ -1,3 +1,4 @@
+from typing import Union
 import pygame
 import KDS.ConfigManager
 import KDS.Logging
@@ -116,6 +117,6 @@ def SetVolume(volume: float):
     for i in range(len(EffectChannels)):
         EffectChannels[i].set_volume(volume)
 
-def PlayFromFile(path, volume: float = -1, loops: int = 0, fade_ms: int = 0) -> pygame.mixer.Channel:
+def PlayFromFile(path: str, volume: float = -1, loops: int = 0, fade_ms: int = 0) -> pygame.mixer.Channel:
     sound = SoundMixer.Sound(path)
     return PlaySound(sound, volume, loops, fade_ms)
