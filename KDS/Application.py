641695c56ea2d2a02cca1077b6289988ec9eeb3d
NAL <niko.a.leinonen@gmail.com>
2021-07-24 17:14:28 +0300

    Update Application.py

--- KDS/Application.py
+++ KDS/Application.py
@@ -3,5 +3,5 @@
 # 0.1.0-BETA
 # 1.0.0-RC69   (RC == Release Candidate)
 # 4.2.0        (Release version does not have anything after it.)
-VERSION: str = "1.0.2"
+VERSION: str = "1.0.5"
 ISDEBUGBUILD: bool = False
