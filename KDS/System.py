91d6d088663514aea45fadd980a233b38b6b7455
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-05-10 19:09:36 +0300

    Update System.py

--- KDS/System.py
+++ KDS/System.py
@@ -219,15 +219,15 @@ class Console:
         return text
 
 class EXTENDED_NAME_FORMAT(IntEnum):
-    NameUnknown = 0,
-    NameFullyQualifiedDN = 1,
-    NameSamCompatible = 2,
-    NameDisplay = 3,
-    NameUniqueId = 6,
-    NameCanonical = 7,
-    NameUserPrincipal = 8,
-    NameCanonicalEx = 9,
-    NameServicePrincipal = 10,
+    NameUnknown = 0
+    NameFullyQualifiedDN = 1
+    NameSamCompatible = 2
+    NameDisplay = 3
+    NameUniqueId = 6
+    NameCanonical = 7
+    NameUserPrincipal = 8
+    NameCanonicalEx = 9
+    NameServicePrincipal = 10
     NameDnsDomain = 12
 
 def GetUserNameEx(NameDisplay: EXTENDED_NAME_FORMAT) -> Optional[str]:
