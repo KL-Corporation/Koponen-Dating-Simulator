933c0e6edf63745e67799ba8ee5adc5079a6b7a3
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-05-17 21:55:42 +0300

    Fix broken git merge
    We need to be more careful when mergin stuff next time

--- KDS/Keys.py
+++ KDS/Keys.py
@@ -182,10 +182,6 @@ class Key(BaseKey):
             return self.secondaryBinding.get_displayname()
         else:
             return "null"
-        
-    @property
-    def Bindings(self) -> Tuple[Binding]:
-        return (self.binding, self.secondaryBinding)
 
 class InventoryKey(Key):
     def __init__(self, defaultBinding: Binding, inventory_index: int) -> None:
