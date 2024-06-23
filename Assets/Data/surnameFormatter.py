9ec1bcaa623d1bbfbfaa3fb710a0deebba149e1c
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-06-23 20:58:11 +0300

    Update surname & forename database

--- Assets/Data/surnameFormatter.py
+++ Assets/Data/surnameFormatter.py
@@ -1,5 +1,5 @@
 # Put a surname Excel file (https://www.avoindata.fi/data/fi/dataset/none) in this Data folder and run this script.
-# Current statistics are from 5.2.2021. The date is marked correctly with day before month.
+# Current statistics are from 1.2.2024 (DD.MM.YYYY)
 
 import os
 import pandas
--- Assets/Data/womenForenameFormatter.py
+++ Assets/Data/womenForenameFormatter.py
@@ -1,5 +1,5 @@
-# Put a forname Excel file (https://www.avoindata.fi/data/fi/dataset/none) in this Data folder and run this script.
-# Current statistics are from 5.2.2021. The date is marked correctly with day before month.
+# Put a forename Excel file (https://www.avoindata.fi/data/fi/dataset/none) in this Data folder and run this script.
+# Current statistics are from 1.2.2024 (DD.MM.YYYY)
 
 import os
 import pandas
@@ -31,10 +31,10 @@ def addCustom(customSurname: str):
     else:
         print(f"Custom surname {customSurname} is already in naisten etunimet!")
 #endregion
-#region Custom Surnames
+#region Custom Forename
     # Currently there is no need to add any
 #endregion
 #region File Saving
-with open("women_fornames.txt", "w", encoding="utf-8") as f:
+with open("women_forenames.txt", "w", encoding="utf-8") as f:
     f.write(newNaistenNimet.strip("\n"))
 #endregion
